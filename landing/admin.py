3a6b656b00c25738064218f9c6f8bb50f9d81d63
Kravtsov Anton <anton-kravtsov@mail.ru>
2018-01-15 00:22:51 +0300

    First commit

---
+++ landing/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+class SubscribierAdmin(admin.ModelAdmin):
+    # list_display = ["name", "email"]
+    list_display = [field.name for field in Subscribier._meta.fields ]
+    # exclude = ('email',) # поле не показывается при выделении
+    fields = ("name",)  # показывается
+    list_filter = ("name",)
+    search_fields = ("name", "email",)
+
+    class Meta:
+        model = Subscribier
+
+admin.site.register(Subscribier, SubscribierAdmin)
---
+++ landing/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Subscribier(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "Пользователь - %s, %s" % (self.name, self.email)
+
+    class Meta:
+        verbose_name = "Подписчики"
+        verbose_name_plural = "Список подписчиков"
