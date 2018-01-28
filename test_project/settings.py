ec752a5fc9522e1cef0c00b9744f6c55c3eb37bc
Anton Kravtsov <anton-kravtsov@mail.ru>
2018-01-28 22:16:12 +0300

    collect static

--- test_project/settings.py
+++ test_project/settings.py
@@ -135,12 +135,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
-try:
-    from .settings_prod import *
-except:
-    pass
+# MEDIA_URL = '/media/'
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+# try:
+#     from .settings_prod import *
+# except:
+#     pass
